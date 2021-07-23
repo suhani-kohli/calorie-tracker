3912b23cdf2fc6f320bb29afd4944f5a8cb405f1
suhani.kohli <suhani.kohli.18@gmail.com>
2021-07-24 00:35:03 +0530

    navpanel styling

--- src/pages/NavPanel/NavPanel.js
+++ src/pages/NavPanel/NavPanel.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import './Styles.css';
 
 const NavPanel = () => {
     return (
         <div>
             <header>
-                <nav>
-                    <ul>
+                <nav className="panel">
+                    <ul className="list">
                         <li>
                             <Link to="/">Diary</Link>
                         </li>
