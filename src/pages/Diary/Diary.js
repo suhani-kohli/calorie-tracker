ee99df6d8665df0ea7e7035edafd407ecf5793b8
suhani.kohli <suhani.kohli.18@gmail.com>
2021-07-24 01:11:00 +0530

    diary page with date

--- src/pages/Diary/Diary.js
+++ src/pages/Diary/Diary.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Styles.css'
 
 const Diary = () => {
+    const selectedDate = "02-11-2019";
     return (
         <div>
-            Diary Page
+            <div className="food-diary">
+                <span className="heading">
+                    <h1 className="headerText">Your food diary for: </h1>
+                    <button className="food-diary-button prev" onClick={() => { }}>
+                        {"<--"}
+                    </button>
+                    <h1 className="Datebutton">{selectedDate}</h1>
+                    <button className="food-diary-button next" onClick={() => { }}>
+                        {"-->"}
+                    </button>
+                </span>
+            </div>
         </div>
     )
 }
