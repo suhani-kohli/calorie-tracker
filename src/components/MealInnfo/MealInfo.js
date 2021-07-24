79faaffe5a6cbe30c1e2963bf91684e888e6d661
suhani.kohli <suhani.kohli.18@gmail.com>
2021-07-24 15:08:04 +0530

    Total calories for a meal row + styling

--- src/components/MealInnfo/MealInfo.js
+++ src/components/MealInnfo/MealInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaRegTrashAlt } from "react-icons/fa";
+import "./Styles.css";
 
 const MealInfo = ({ date, mealName }) => {
   const diary = [
@@ -28,6 +29,20 @@ const MealInfo = ({ date, mealName }) => {
     },
   ];
 
+  let totalKcal = 0,
+    totalProtein = 0,
+    totalCarbs = 0,
+    totalFat = 0;
+
+  const setAllMealTotals = food => {
+    // calculating total calories and macros for a meal
+    const { calories, protein, fat, carbs } = food;
+    totalKcal += calories;
+    totalProtein += protein;
+    totalCarbs += carbs;
+    totalFat += fat;
+  };
+
 
   const handleOnDeleteClick = id => {
     let confirmation = window.confirm(
@@ -51,13 +66,15 @@ const MealInfo = ({ date, mealName }) => {
           {food.mealName === mealName ? (
             <>
               <td className="meal">
+                {setAllMealTotals(food)}
                 {`${food.foodItem}, ${food.amount}
                   ${food.size !== "" && food.size !== "0" ? " " + food.size : ""
                   }`}
               </td>
-              <td className="macros">{food.protein}</td>
-              <td className="macros">{food.carbs}</td>
-              <td className="macros">{food.fat}</td>
+              <td className="macros">{food.calories}</td>
+              <td className="macros">{food.protein.toFixed(0)}</td>
+              <td className="macros">{food.carbs.toFixed(0)}</td>
+              <td className="macros">{food.fat.toFixed(0)}</td>
               <td
                 className="delete"
                 onClick={() => handleOnDeleteClick(food.id)}
@@ -67,13 +84,20 @@ const MealInfo = ({ date, mealName }) => {
             </>
           ) : null}
         </tr>
-      ))}
+      ))
+      }
 
-      <tr className="bottom-bar">
+      <tr tr className="bottom-bar">
         <td className="add-food">
           <Link to="/addfood">Add food</Link>
         </td>
+        <td className="totals">{totalKcal.toFixed(0)}</td>
+        <td className="totals">{totalProtein.toFixed(0)}</td>
+        <td className="totals">{totalCarbs.toFixed(0)}</td>
+        <td className="totals">{totalFat.toFixed(0)}</td>
+        <tr />
       </tr>
+
     </>
   );
 };
